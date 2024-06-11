b90d49daaf3e090fea3c8e72a608357f8ee8595b
Manojkumar962952 <126284997+Manojkumar962952@users.noreply.github.com>
2024-06-11 19:50:31 +0530

    Update HelloWorld.java

--- javacode/HelloWorld.java
+++ javacode/HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.*;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int age=58;
+        if(age>=18){
+            printf("You are eligible for vote"); 
+        }else{
+            printf("You are not eligible for vote");
+        }
     }
 }
