2b8f2f30b57f7e4425ee65431d8e817749355902
Manojkumar962952 <126284997+Manojkumar962952@users.noreply.github.com>
2024-06-11 20:08:49 +0530

    Update HelloWorld.java

--- javacode/HelloWorld.java
+++ javacode/HelloWorld.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class HelloWorld {
     public static void main(String[] args) {
-        int age=58;
+        int age=12;
         if(age>=18){
             System.out.println("You are eligible for vote"); 
         }else{
